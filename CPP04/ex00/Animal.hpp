333ae434eacdadb7305829afe1d6d564b7fe0b23
Robin-Lenz Podack <rpodack@c1b3c3.42berlin.de>
2024-03-07 17:11:54 +0100

    started cpp04

---
+++ CPP04/ex00/Animal.hpp
@@ -0,0 +1,20 @@
+#ifndef ANIMAL_H
+# define ANIMAL_H
+
+#include <string>
+
+class Animal
+{
+	private:
+		/* data */
+	
+	protected:
+		std::string type;
+
+	public:
+		Animal(/* args */);
+		~Animal();
+};
+
+
+#endif
